fa72c216401cf95d32796cfe3bea8732576f7510
minhlq2311 <minhlq2311@gmail.com>
2025-06-06 06:15:49 +0700

    Add pagination and search function

--- api/v1/controllers/task.controller.js
+++ api/v1/controllers/task.controller.js
@@ -1,9 +1,36 @@
 // function to get all tasks
 const Task = require('../models/task.model');
-
+const paginationHelper = require('../../../helpers/pagination.js');
+const searchHelper = require('../../../helpers/search.js');
 const index = async (req, res) => {
     try {
-        const tasks = await Task.find({ deleted: false });
+        const find = {
+            deleted: false
+        }
+        // Filter status 
+        if(req.query.status){
+            find.status = req.query.status;
+        }
+
+        // Pagination
+        const countRecords = await Task.countDocuments(find);
+        let objectPagination = paginationHelper({
+            limitItems: 2,
+            currentPage: 1,
+        }, req.query, countRecords);
+
+        // Search
+        const objectSearch = searchHelper(req.query);
+        if(objectSearch.regex){
+            find.title = objectSearch.regex;
+        }
+        // Sort
+        const sort = {};
+        if(req.query.sortKey && req.query.sortValue){
+            sort[req.query.sortKey] = req.query.sortValue
+        }
+        const tasks = await Task.find(find).sort(sort).skip(objectPagination.skip).limit(objectPagination.limitItems);
+
         res.json(tasks);
     } catch (error) {
         res.status(500).json({ message: 'Error fetching tasks' });
--- api/v1/routes/task.route.js
+++ api/v1/routes/task.route.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const controller = require('../controllers/task.controller');
 
 
-router.get('/all', controller.index);
+router.get('/', controller.index);
 router.get('/details/:id', controller.details);
 
 module.exports = router;
