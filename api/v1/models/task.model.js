63e0e91682d3553cdecc30f95351fdc4bd2d93a6
minhlq2311 <minhlq2311@gmail.com>
2025-06-07 06:04:03 +0700

    Add child task

--- api/v1/models/task.model.js
+++ api/v1/models/task.model.js
@@ -8,6 +8,7 @@ const taskSchema = new mongoose.Schema({
     timeFinish: Date,
     createdBy: String,
     listUsers: Array,
+    taskParentId: String,
     deleted: {
         type: Boolean,
         default: false
